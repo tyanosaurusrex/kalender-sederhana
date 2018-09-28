431742ddd3e56a1d6f4892f70330a0f6543f955f
anjar <anjar@skyshi.io>
2018-09-28 13:56:16 +0700

    first

---
+++ script.js
@@ -0,0 +1,71 @@
+var monthField = document.querySelector('.monthField');
+var yearField = document.querySelector('.yearField');
+var submit = document.querySelector('.submit');
+var calendar = document.querySelector('.calendar');
+calendar.style.whiteSpace = 'pre';
+
+var days = ['Senin', 'Selasa', 'Rabu', 'Kamis', 'Jumat', 'Sabtu', 'Minggu'];
+
+function generateCalendar(){
+  var month = monthField.value;
+  var year = yearField.value;
+  var dayArray = [];
+
+  if (year === ''){
+    alert('Pilih bulan dan masukkan tahun');
+  } else {
+    var daysInMonth = new Date(year, month, 0).getDate();
+    var firstDay = new Date(year+'-'+month+'-'+1).getDay();
+
+    firstDay = (firstDay != 0) ? firstDay - 1 : 6;
+
+    var countDate = 1;
+    var lastMonth = new Date(year, month-1, 0).getDate();
+    var lastMonthInWeek = lastMonth - firstDay + 1;
+    var weeks = Math.ceil((daysInMonth - (days.length - firstDay))/7) + 1;
+    
+    for (i=1; i<=weeks; i++){
+      dayArray[i] = [];
+      for (j=0; j<days.length; j++){
+        if (firstDay !== 0){
+          if (lastMonthInWeek <= lastMonth){
+            dayArray[i][j] = lastMonthInWeek;
+            lastMonthInWeek++;
+          } else {
+            if (countDate <= daysInMonth){
+              dayArray[i][j] = countDate;
+            } else {
+              dayArray[i][j] = countDate%daysInMonth;
+            }
+            countDate++;
+          }
+        } else {
+          if (countDate <= daysInMonth){
+            dayArray[i][j] = countDate;
+          } else {
+            dayArray[i][j] = countDate%daysInMonth;
+          }
+          countDate++;
+        }
+      }
+      }
+    }
+    render(weeks, dayArray);
+}
+
+function render(weeks, dayArray){
+  var show = '';
+  for (var i=0; i<=weeks; i++){
+    for (var j=0; j<days.length; j++){
+      if (i === 0){
+        show += String(days[j]).padStart(7, ' ') + ' ';
+      } else {
+        show += String(dayArray[i][j]).padStart(7, ' ') + ' ';
+      }
+    }
+    show += '\n';
+  }
+  calendar.textContent = show;
+}
+
+submit.addEventListener('click', generateCalendar);
